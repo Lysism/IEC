a50d4860c1f24fc7033540dffb7392c0f6e1e3ef
Ari Lotter <arilotter@gmail.com>
2017-11-19 13:51:14 -0500

    server works now

--- src/server.py
+++ src/server.py
@@ -1,12 +1,13 @@
 from flask import Flask, send_from_directory, request, jsonify
 app = Flask(__name__)
 
-# @app.route('/', methods=['GET', 'POST'])
-# def main():
-#     if request.method == 'GET':
-#         return app.send_static_file('./frontend/index.html')
-#     return jsonify({"hello": "world"})
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return send_from_directory('frontend', 'index.html')
+    return jsonify({"hello": "world"})
 
-@app.route('/<path:path>')
-def send(path):
-    return send_from_directory('frontend', path)
+@app.route('/<path:file>')
+def send(file):
+    print(file)
+    return send_from_directory('frontend', file)
